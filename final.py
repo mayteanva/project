d5ae0f9f494bcd965f0beba855b18f216db66115
mayteanva <150102176+mayteanva@users.noreply.github.com>
2024-05-09 22:45:28 +0200

    Update final.py

--- final.py
+++ final.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1-c3rA6vMncx0Ixb89smsFBwG7tHhVXya
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
