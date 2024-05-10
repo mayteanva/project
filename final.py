71e2771006b9dbd33d89cd2d8c6ead07290d6ae7
mayteanva <150102176+mayteanva@users.noreply.github.com>
2024-05-10 19:21:37 +0200

    Update final.py

--- final.py
+++ final.py
@@ -61,6 +61,26 @@ country_acronyms = {
 country = st.selectbox('Choose a country', sorted(country_acronyms.keys()))
 st.write(f'You have chosen {country}')
 
+conn = sqlite3.connect('ecsel_database.db')
+df_yearly_contributions = pd.read_sql(f"""SELECT strftime('%Y', p.startDate) AS Year, SUM(p.ecContribution) AS ECContribution
+                                                FROM participants AS p
+                                                JOIN countries AS c
+                                                ON c.Acronym = p.country
+                                                WHERE c.Country = '{country}'
+                                                GROUP BY Year
+                                                ORDER BY Year ASC""", conn)
+conn.close()
+
+st.markdown(f'<h2 style="color: lightsteelblue;">Yearly EC Contribution in {country}</h2>', unsafe_allow_html=True)
+plt.figure(figsize=(10, 6))
+sns.barplot(data=df_yearly_contributions, x='Year', y='ECContribution', palette='coolwarm')
+plt.xticks(rotation=45)
+plt.xlabel('Year')
+plt.ylabel('EC Contribution (€)')
+plt.title(f'Yearly EC Contribution in {country}', color='steelblue')
+st.pyplot(plt.gcf())
+
+
  # Display itin 2 colums:
 col1, col2 = st.columns(2)
 
