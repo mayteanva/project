f5b7b761ecae90e8579f654cc5bff0f4a7785935
mayteanva <150102176+mayteanva@users.noreply.github.com>
2024-05-10 22:33:27 +0200

    Update final.py

--- final.py
+++ final.py
@@ -97,7 +97,7 @@ conn.close()
 
 conn = sqlite3.connect('ecsel_database.db')
 activity = pd.read_sql(f"""SELECT activityType FROM Participants ORDER BY activityType""", conn)
-activity_types= st.multiselect('Select activity types', activity.unique())
+activity_types= st.multiselect('Select activity types', activity['activityType'].tolist())
 conn.close()
 
 
